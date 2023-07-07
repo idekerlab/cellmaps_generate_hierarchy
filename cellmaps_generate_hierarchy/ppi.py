284b3297fb012de499596eac871d3504efaf8cbf
Chris Churas <churas.camera@gmail.com>
2023-07-07 16:26:02 -0700

    wrapped cutoffs values for style

--- cellmaps_generate_hierarchy/ppi.py
+++ cellmaps_generate_hierarchy/ppi.py
@@ -43,7 +43,9 @@ class CosineSimilarityPPIGenerator(PPINetworkGenerator):
     """
 
     def __init__(self, embeddingdir=None,
-                 cutoffs=[0.001, 0.002, 0.003, 0.004, 0.005, 0.006, 0.007, 0.008, 0.009, 0.01, 0.02, 0.03, 0.04, 0.05, 0.10]):
+                 cutoffs=[0.001, 0.002, 0.003, 0.004, 0.005,
+                          0.006, 0.007, 0.008, 0.009, 0.01,
+                          0.02, 0.03, 0.04, 0.05, 0.10]):
         """
         Constructor
         """
