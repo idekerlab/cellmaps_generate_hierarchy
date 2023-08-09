eb1ac86fa64770fd159d29034deda8fc9e6da89a
Chris Churas <churas.camera@gmail.com>
2023-08-08 17:04:34 -0700

    added requests library

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = ['cellmaps_utils',
+                'requests',
                 'tqdm',
                 'pandas',
                 'numpy',
