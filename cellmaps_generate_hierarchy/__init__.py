760d1e07968b5665006703123256e8d0f030e91b
Chris Churas <churas.camera@gmail.com>
2023-08-16 17:28:29 -0700

    bumped version

--- cellmaps_generate_hierarchy/__init__.py
+++ cellmaps_generate_hierarchy/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-"""Top-level package for CM4AI Generate Hierarchy."""
+"""Top-level package for Cell Maps Generate Hierarchy."""
 
-__author__ = 'Ideker Lab CM4AI team'
+__author__ = 'Ideker Lab Cell Maps team'
 __email__ = 'tools@cm4ai.org'
-__version__ = '0.1.0a8'
+__version__ = '0.1.0a9'
 __repo_url__ = 'https://github.com/idekerlab/cellmaps_generate_hierarchy'
 __description__ = 'A tool to generate hierarchies from protein to protein interaction networks'
