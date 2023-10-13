a90ef33919158e2b3b9dfa09b46d54ddb62debf8
dzoanka <joanna.lenkiewicz@gmail.com>
2023-10-13 14:12:55 -0700

    Skip_logging flag set to True by default (fix for UD-2617)

--- tests/test_cellmapsgeneratehierarchy.py
+++ tests/test_cellmapsgeneratehierarchy.py
@@ -7,6 +7,10 @@ import os
 import shutil
 import tempfile
 import unittest
+
+from cellmaps_utils.exceptions import CellMapsProvenanceError
+
+from cellmaps_generate_hierarchy.exceptions import CellmapsGenerateHierarchyError
 from cellmaps_generate_hierarchy.runner import CellmapsGenerateHierarchy
 
 
@@ -28,3 +32,48 @@ class TestCellmapsgeneratehierarchyrunner(unittest.TestCase):
         finally:
             shutil.rmtree(temp_dir)
 
+    def test_constructor_outdir_must_be_set(self):
+        """Tests constructor outdir must be set"""
+        try:
+            CellmapsGenerateHierarchy()
+            self.fail('Expected exception')
+        except CellmapsGenerateHierarchyError as e:
+            self.assertEqual('outdir is None', str(e))
+
+    def test_run_without_logging(self):
+        """ Tests run() without logging."""
+        temp_dir = tempfile.mkdtemp()
+        try:
+            run_dir = os.path.join(temp_dir, 'run')
+            myobj = CellmapsGenerateHierarchy(outdir=run_dir)
+            try:
+                myobj.run()
+                self.fail('Expected CellMapsProvenanceError')
+            except CellMapsProvenanceError as e:
+                print(e)
+                self.assertTrue('rocrates' in str(e))
+
+            self.assertFalse(os.path.isfile(os.path.join(run_dir, 'output.log')))
+            self.assertFalse(os.path.isfile(os.path.join(run_dir, 'error.log')))
+
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_run_with_logging(self):
+        """ Tests run() with logging."""
+        temp_dir = tempfile.mkdtemp()
+        try:
+            run_dir = os.path.join(temp_dir, 'run')
+            myobj = CellmapsGenerateHierarchy(outdir=run_dir,
+                                              skip_logging=False)
+            try:
+                myobj.run()
+                self.fail('Expected CellMapsProvenanceError')
+            except CellMapsProvenanceError as e:
+                self.assertTrue('rocrates' in str(e))
+
+            self.assertTrue(os.path.isfile(os.path.join(run_dir, 'output.log')))
+            self.assertTrue(os.path.isfile(os.path.join(run_dir, 'error.log')))
+
+        finally:
+            shutil.rmtree(temp_dir)
