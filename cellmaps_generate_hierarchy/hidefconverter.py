df2aab8993855c7d26f6a549fd37d4f02f9577ab
dzoanka <joanna.lenkiewicz@gmail.com>
2024-01-11 19:06:09 -0800

    Add docstrings

--- cellmaps_generate_hierarchy/hidefconverter.py
+++ cellmaps_generate_hierarchy/hidefconverter.py
@@ -10,9 +10,18 @@ logger = logging.getLogger(__name__)
 
 
 class HierarchyToHiDeFConverter:
+    """
+    A class to convert a hierarchy network (in CX2 format) to a HiDeF format.
+    """
     HIDEF_OUT_PREFIX = 'hidef_output_gene_names'
 
     def __init__(self, directory):
+        """
+        Constructor
+
+        :param directory: The directory containing the hierarchy file and where the output files will be stored.
+        :type directory: str
+        """
         self.directory = directory
         self.hierarchy_path = os.path.join(directory,
                                            cellmaps_constants.HIERARCHY_NETWORK_PREFIX + cellmaps_constants.CX2_SUFFIX)
@@ -24,6 +33,9 @@ class HierarchyToHiDeFConverter:
             raise CellmapsGenerateHierarchyError(f"Failed to load hierarchy: {e}")
 
     def generate_hidef_files(self):
+        """
+        Generates HiDeF files .nodes and .edges from the hierarchy network.
+        """
         try:
             nodes = self.hierarchy.get_nodes()
             edges = self.hierarchy.get_edges()
@@ -37,9 +49,27 @@ class HierarchyToHiDeFConverter:
 
     @staticmethod
     def _format_aspect(aspect, format_function):
+        """
+        Formats aspect list (nodes or edges) using a specified format function.
+
+        :param aspect: A list of aspect IDs (nodes or edges).
+        :type aspect: list
+        :param format_function: A function to format a single entity.
+        :type format_function: function
+        :return: A list of formatted nodes or edges.
+        :rtype: list
+        """
         return [format_function(aspect_id) for aspect_id in aspect]
 
     def _format_node(self, node_id):
+        """
+        Formats a node for HiDeF output.
+
+        :param node_id: The ID of the node to be formatted.
+        :type node_id: int
+        :return: A formatted string representing the node.
+        :rtype: str
+        """
         node = self.hierarchy.get_node(node_id)
         node_attr = node[constants.ASPECT_VALUES]
         formatted_node = (node_attr['name'], node_attr['CD_MemberList_Size'],
@@ -47,16 +77,40 @@ class HierarchyToHiDeFConverter:
         return "\t".join(map(str, formatted_node))
 
     def _format_edge(self, edge_id):
+        """
+        Formats an edge for HiDeF output.
+
+        :param edge_id: The ID of the edge to be formatted.
+        :type edge_id: int
+        :return: A formatted string representing the edge.
+        :rtype: str
+        """
         edge = self.hierarchy.get_edge(edge_id)
         source_node = self._find_node_name_by_id(edge[constants.EDGE_SOURCE])
         target_node = self._find_node_name_by_id(edge[constants.EDGE_TARGET])
         return f"{source_node}\t{target_node}\tdefault"
 
     def _find_node_name_by_id(self, node_id):
+        """
+        Finds the name of a node given its ID.
+
+        :param node_id: The ID of the node.
+        :type node_id: int
+        :return: The name of the node.
+        :rtype: str
+        """
         node = self.hierarchy.get_node(node_id)
         return node[constants.ASPECT_VALUES]['name']
 
     def _write_to_file(self, filename, lines):
+        """
+        Writes output to a file.
+
+        :param filename: The name of the file to write to.
+        :type filename: str
+        :param lines: A list of strings to be written to the file.
+        :type lines: list
+        """
         file_path = os.path.join(self.directory, filename)
         with open(file_path, 'w') as file:
             file.write('\n'.join(lines))
